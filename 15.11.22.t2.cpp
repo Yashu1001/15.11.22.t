13654befc024ba61f4d6aa1b1e143ea5ff01ab9a
Yashu1001 <118185290+Yashu1001@users.noreply.github.com>
2022-11-15 03:31:49 -1000

    Add files via upload

---
+++ 15.11.22.t2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	
+	int a=10;
+	int b=20;
+	int c;
+	
+	c=(a*a)+(b*b)-(2*a*b);
+	
+	printf("%i",c);
+	
+}
---
+++ 15.11.22.t3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	
+	int a=10;
+	int b=20;
+	int c;
+	
+	c=(a*a*a)+(b*b*b)+(3*a*a*b)+(3*b*b*a);
+	 printf("%i",c);
+	 
+	
+}
